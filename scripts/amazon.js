624e79b20ffb43be14da5ac58ba4ab49745396e0
yashvardhan <yashvardhancn44@gmail.com>
2024-09-05 08:42:08 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,6 +78,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((buttonElement) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
