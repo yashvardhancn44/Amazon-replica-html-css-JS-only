69050030046e598b15dfa485fb9f037ca4bf7ed9
yashvardhan <yashvardhancn44@gmail.com>
2024-09-07 05:08:45 +0530

    Create automated(integrationTest) for OrderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -30,7 +30,7 @@ export function renderOrderSummary() {
     const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
-          <div class="cart-item-container js-cart-item-container-${
+          <div class="cart-item-container js-cart-item-container js-cart-item-container-${
             matchingProduct.id
           }">
             <div class="delivery-date">
@@ -47,7 +47,9 @@ export function renderOrderSummary() {
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${
+                  matchingProduct.id
+                }">
                   <span>
                     Quantity: <span class="quantity-label js-quantity-label js-quantity-label-${
                       matchingProduct.id
@@ -64,9 +66,9 @@ export function renderOrderSummary() {
                   }">
                   <span class="save-quantity-link js-save-quantity-link link-primary" >Save</span> 
                   
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                  <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${
                     matchingProduct.id
-                  }">
+                  }" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>                
                 </div>
---
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,75 @@
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+import { loadFromStorage, cart } from "../../data/cart.js";
+
+describe("test suite: renderOrderSummary", () => {
+  const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+
+  beforeEach(() => {
+    spyOn(localStorage, "setItem");
+
+    document.querySelector(".js-test-container").innerHTML = `
+    <div class="js-order-summary">
+    </div>
+    <div class="js-payment-summary"></div>
+    <div class="js-checkout-header-middle-section"></div>
+    
+    `;
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ]);
+    });
+    loadFromStorage();
+
+    renderOrderSummary();
+  });
+
+  afterEach(() => {
+    document.querySelector(".js-test-container").innerHTML = ``; // to remove the HTML after we run the test.
+  });
+
+  //TESTING HOW THE PAGELOOKS
+  it("displays the cart", () => {
+    expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
+      2
+    );
+
+    expect(
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText
+    ).toContain("Quantity: 2");
+    expect(
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText
+    ).toContain("Quantity: 1");
+  });
+
+  //TESTING HOW THE PAGE BEHAVES
+  it("removes a product", () => {
+    document.querySelector(`.js-delete-link-${productId1}`).click();
+
+    expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
+      1
+    );
+
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId1}`)
+    ).toEqual(null);
+
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId2}`)
+    ).not.toEqual(null);
+
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+  });
+});
